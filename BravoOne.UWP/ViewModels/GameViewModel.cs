f225b946b527f800a66d3b6fc9b6c47d43cb3cc2
Jarred Capellman <jcapellman@hotmail.com>
2021-09-25 10:31:02 -0500

    Added: Date Incrementer logic

--- BravoOne.UWP/ViewModels/GameViewModel.cs
+++ BravoOne.UWP/ViewModels/GameViewModel.cs
@@ -1,8 +1,46 @@
-﻿using BravoOne.UWP.ViewModels.Base;
+﻿using System;
+
+using BravoOne.UWP.ViewModels.Base;
 
 namespace BravoOne.UWP.ViewModels
 {
     public class GameViewModel : BaseViewModel
     {
+        private string _currentDateString;
+
+        public string CurrentDateString
+        {
+            get => _currentDateString;
+
+            set
+            {
+                _currentDateString = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private DateTime _currentDate;
+
+        public DateTime CurrentDate
+        {
+            get => _currentDate;
+
+            set
+            {
+                _currentDate = value;
+
+                CurrentDateString = $"{_currentDate:MMMM} {_currentDate.Year}";
+                OnPropertyChanged();
+            }
+        }
+        public GameViewModel()
+        {
+            CurrentDate = DateTime.Now;
+        }
+
+        public void EndMonth()
+        {
+            CurrentDate = CurrentDate.AddMonths(1);
+        }
     }
 }
