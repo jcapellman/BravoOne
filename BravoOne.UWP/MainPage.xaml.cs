719e027ff84a337d8073e49319cb17db5dcf6f31
Jarred Capellman <jcapellman@hotmail.com>
2021-09-25 10:30:21 -0500

    Added: EndMonth button wire

--- BravoOne.UWP/MainPage.xaml.cs
+++ BravoOne.UWP/MainPage.xaml.cs
@@ -1,12 +1,21 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using BravoOne.UWP.ViewModels;
+
+using Windows.UI.Xaml.Controls;
 
 namespace BravoOne.UWP
 {
     public sealed partial class MainPage : Page
     {
+        private GameViewModel ViewModel => (GameViewModel)DataContext;
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void btnEndMonth_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            ViewModel.EndMonth();
         }
     }
 }
