3f103e41b32da8ed2cff467a6d54305a3a5666b4
Jarred Capellman <jcapellman@hotmail.com>
2021-09-26 20:11:14 -0500

    Added: Initial work on the TeamMember class

---
+++ BravoOne.lib/Objects/TeamMember.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BravoOne.lib.Objects
+{
+    public class TeamMember
+    {
+        public string Name { get; set; }
+
+        public int MonthlySalary { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public int Health { get; set; }
+
+        public int Status { get; set; }
+    }
+}
