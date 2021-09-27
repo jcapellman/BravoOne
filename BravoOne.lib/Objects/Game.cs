823354910cd586d27cd2de0b3b357ce1be396bc8
Jarred Capellman <jcapellman@hotmail.com>
2021-09-26 20:10:46 -0500

    Added: Initial work on the Game Object

---
+++ BravoOne.lib/Objects/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BravoOne.lib.Objects
+{
+    public class Game
+    {
+        public List<TeamMember> TeamMembers { get; set; }
+
+        public DateTime CurrentDate { get; set; }
+
+        public Game()
+        {
+            CurrentDate = DateTime.Now;
+        }
+
+        public void EndTurn()
+        {
+            CurrentDate = CurrentDate.AddMonths(1);
+        }
+    }
+}
