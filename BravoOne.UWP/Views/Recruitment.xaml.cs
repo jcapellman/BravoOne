9bfbe8c03c9d081c961ebec87536f91922608e0b
Jarred Capellman <jcapellman@hotmail.com>
2021-09-29 18:46:18 -0500

    Added: Recruitment View

---
+++ BravoOne.UWP/Views/Recruitment.xaml.cs
@@ -0,0 +1,28 @@
+﻿using BravoOne.lib.Objects;
+
+using BravoOne.UWP.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace BravoOne.UWP.Views
+{
+    public sealed partial class Recruitment : Page
+    {
+        private RecruitmentViewModel ViewModel => (RecruitmentViewModel)DataContext;
+
+        public Recruitment()
+        {
+            InitializeComponent();
+
+            DataContext = new RecruitmentViewModel();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            ViewModel.CurrentGame = (Game)e.Parameter;
+
+            base.OnNavigatedTo(e);
+        }
+    }
+}
