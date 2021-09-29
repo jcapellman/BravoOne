28e3e91abf5e4059a2312f82f25fadb58f164384
Jarred Capellman <jcapellman@hotmail.com>
2021-09-29 18:45:14 -0500

    Added: RecruitmentViewModel

---
+++ BravoOne.UWP/ViewModels/RecruitmentViewModel.cs
@@ -0,0 +1,23 @@
+﻿using BravoOne.lib.Objects;
+
+using BravoOne.UWP.ViewModels.Base;
+
+namespace BravoOne.UWP.ViewModels
+{
+    public class RecruitmentViewModel : BaseViewModel
+    {
+        private Game _game;
+
+        public Game CurrentGame
+        {
+            get => _game;
+
+            set
+            {
+                _game = value;
+
+                OnPropertyChanged();
+            }
+        }
+    }
+}
