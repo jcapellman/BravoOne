897802adcab3ab6dba2cec77bcd34b05c0a5c85b
Jarred Capellman <jcapellman@hotmail.com>
2021-10-01 18:56:44 -0500

    Added: BaseMVVM class

---
+++ BravoOne.lib/Objects/Base/BaseMVVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BravoOne.lib.Objects.Base
+{
+    public class BaseMVVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
