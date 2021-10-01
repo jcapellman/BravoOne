61051cb027b096b1d0b8aa377a7529c962536126
Jarred Capellman <jcapellman@hotmail.com>
2021-09-30 19:03:03 -0500

    Change: Removed duplicate code from the GameViewModel class

--- BravoOne.UWP/ViewModels/GameViewModel.cs
+++ BravoOne.UWP/ViewModels/GameViewModel.cs
@@ -5,57 +5,14 @@ namespace BravoOne.UWP.ViewModels
 {
     public class GameViewModel : BaseViewModel
     {
-        private string _currentDateString;
-
-        public string CurrentDateString
-        {
-            get => _currentDateString;
-
-            set
-            {
-                _currentDateString = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private Game _game;
-
-        public Game CurrentGame
-        {
-            get => _game;
-
-            set
-            {
-                _game = value;
-
-                OnPropertyChanged();
-            }
-        }
-
         public GameViewModel()
         {
             CurrentGame = new Game();
-
-            UpdateDate();
-        }
-
-        private void UpdateDate()
-        {
-            CurrentDateString = $"{CurrentGame.CurrentDate:MMMM} {CurrentGame.CurrentDate.Year}";
-        }
-
-        public void UpdateGame(Game game)
-        {
-            CurrentGame = game;
-
-            UpdateDate();
         }
 
         public void EndMonth()
         {
             CurrentGame.EndTurn();
-
-            UpdateDate();
         }
     }
 }
