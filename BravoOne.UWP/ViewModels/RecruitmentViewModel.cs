146b1c8823c1770c244d0f41246f23361dd37e62
Jarred Capellman <jcapellman@hotmail.com>
2021-09-30 19:04:53 -0500

    Change: Removed Duplicate code from the RecruitmentViewModel class

--- BravoOne.UWP/ViewModels/RecruitmentViewModel.cs
+++ BravoOne.UWP/ViewModels/RecruitmentViewModel.cs
@@ -1,23 +1,8 @@
-﻿using BravoOne.lib.Objects;
-
-using BravoOne.UWP.ViewModels.Base;
+﻿using BravoOne.UWP.ViewModels.Base;
 
 namespace BravoOne.UWP.ViewModels
 {
     public class RecruitmentViewModel : BaseViewModel
     {
-        private Game _game;
-
-        public Game CurrentGame
-        {
-            get => _game;
-
-            set
-            {
-                _game = value;
-
-                OnPropertyChanged();
-            }
-        }
     }
 }
