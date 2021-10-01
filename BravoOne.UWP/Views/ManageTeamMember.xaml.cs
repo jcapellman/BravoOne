cf0b83a8e2212d1a53c86fa79da31e72e89de172
Jarred Capellman <jcapellman@hotmail.com>
2021-10-01 18:56:25 -0500

    Added: ManageTeamMember View

---
+++ BravoOne.UWP/Views/ManageTeamMember.xaml.cs
@@ -0,0 +1,32 @@
+﻿using BravoOne.lib.Objects;
+using BravoOne.UWP.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace BravoOne.UWP.Views
+{
+    public sealed partial class ManageTeamMember : Page
+    {
+        private ManageTeamMemberViewModel ViewModel => (ManageTeamMemberViewModel)DataContext;
+
+        public ManageTeamMember()
+        {
+            InitializeComponent();
+
+            DataContext = new ManageTeamMemberViewModel();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            ViewModel.CurrentGame = (Game)e.Parameter;
+
+            base.OnNavigatedTo(e);
+        }
+
+        private void btnBack_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage), ViewModel.CurrentGame);
+        }
+    }
+}
