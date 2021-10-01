7ead8199244851f02b956e3a8ebac80c5d7aa8fd
Jarred Capellman <jcapellman@hotmail.com>
2021-09-30 19:01:12 -0500

    Change: Refactored the BaseViewModel

--- BravoOne.UWP/ViewModels/Base/BaseViewModel.cs
+++ BravoOne.UWP/ViewModels/Base/BaseViewModel.cs
@@ -1,10 +1,31 @@
-﻿using System.ComponentModel;
+﻿using BravoOne.lib.Objects;
+
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace BravoOne.UWP.ViewModels.Base
 {
     public class BaseViewModel : INotifyPropertyChanged
     {
+        private Game _game;
+
+        public Game CurrentGame
+        {
+            get => _game;
+
+            set
+            {
+                _game = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public void UpdateGame(Game game)
+        {
+            CurrentGame = game;
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
