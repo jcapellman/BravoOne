3ac9fa04d1248e2bea80b9cc5e5b1734e91f1716
Jarred Capellman <jcapellman@hotmail.com>
2021-10-02 10:00:31 -0500

    Added: Id and SkillPoints properties to the TeamMember class

--- BravoOne.lib/Objects/TeamMember.cs
+++ BravoOne.lib/Objects/TeamMember.cs
@@ -4,6 +4,8 @@ namespace BravoOne.lib.Objects
 {
     public class TeamMember
     {
+        public Guid Id { get; set; }
+
         public string Name { get; set; }
 
         public uint MonthlySalary { get; set; }
@@ -13,5 +15,7 @@ namespace BravoOne.lib.Objects
         public int Health { get; set; }
 
         public int Status { get; set; }
+
+        public int SkillPoints { get; set; }
     }
 }
