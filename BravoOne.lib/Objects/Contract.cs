5f96feed07a5f3786ee8b54029a4f1f7bcd6f8e1
Jarred Capellman <jcapellman@hotmail.com>
2021-10-02 10:01:50 -0500

    Added: Contract Class

---
+++ BravoOne.lib/Objects/Contract.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using BravoOne.lib.Enums;
+
+namespace BravoOne.lib.Objects
+{
+    public class Contract
+    {
+        public Guid Id { get; set; }
+
+        public ulong Income { get; set; }
+
+        public ulong Penalty { get; set; }
+
+        public string Name { get; set; }
+
+        public ContractStatus Status { get; set; }
+
+        public string CompletedDateString { get; set; }
+
+        public DateTime CompleteDate { get; set; }
+
+        public int TeamMemberToll { get; set; }
+
+        public int SkillPointsRemaining { get; set; }
+
+        public List<Guid> AssignedTeamMembers { get; set; }
+
+        internal List<TeamMember> EndTurn(DateTime currentDate, Dictionary<Guid, TeamMember> teamMembers)
+        {
+            if (Status != ContractStatus.InProgress)
+            {
+                return teamMembers.Values.ToList();
+            }
+
+            foreach (Guid guid in AssignedTeamMembers)
+            {
+                SkillPointsRemaining -= teamMembers[guid].SkillPoints;
+                teamMembers[guid].Status -= TeamMemberToll;
+            }
+
+            if (SkillPointsRemaining <= 0)
+            {
+                Status = ContractStatus.Completed;
+            } else if (currentDate > CompleteDate)
+            {
+                Status = ContractStatus.Failed;
+            }
+
+            return teamMembers.Values.ToList();
+        }
+    }
+}
