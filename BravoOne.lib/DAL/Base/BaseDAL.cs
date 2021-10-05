ec2e424e0b221b4ebc26f798d7708893596003e4
Jarred Capellman <jcapellman@hotmail.com>
2021-10-04 19:11:48 -0500

    Added: BaseDAL Definition

---
+++ BravoOne.lib/DAL/Base/BaseDAL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace BravoOne.lib.DAL.Base
+{
+    public abstract class BaseDAL
+    {
+        public abstract void Add<T>(T item);
+
+        public abstract void Update<T>(T item);
+
+        public abstract void Delete<T>(int id);
+
+        public abstract void Delete<T>(Expression<Func<T, bool>> expression);
+
+        public abstract T Get<T>(Expression<Func<T, bool>> expression);
+
+        public abstract List<T> GetAll<T>(Expression<Func<T, bool>> expression);
+    }
+}
