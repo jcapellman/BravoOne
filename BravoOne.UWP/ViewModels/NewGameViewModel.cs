bd22ffd8b137a3c900a72ebca15ce6b402c10980
Jarred Capellman <jcapellman@hotmail.com>
2021-10-14 18:48:30 -0500

    Added: NewGameViewModel class

---
+++ BravoOne.UWP/ViewModels/NewGameViewModel.cs
@@ -0,0 +1,12 @@
+﻿using BravoOne.lib.DAL.Base;
+using BravoOne.UWP.ViewModels.Base;
+
+namespace BravoOne.UWP.ViewModels
+{
+    public class NewGameViewModel : BaseViewModel
+    {
+        public NewGameViewModel(BaseDAL dal) : base(dal)
+        {
+        }
+    }
+}
