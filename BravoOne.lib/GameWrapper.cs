bf1efd86e4bd8adffd50bb9040e383015d1f53b7
Jarred Capellman <jcapellman@hotmail.com>
2021-10-16 18:01:41 -0500

    Added: Initial work on the GameWrapper class

---
+++ BravoOne.lib/GameWrapper.cs
@@ -0,0 +1,22 @@
+﻿using BravoOne.lib.DAL.Base;
+using BravoOne.lib.Objects;
+
+namespace BravoOne.lib
+{
+    public class GameWrapper
+    {
+        public BaseDAL DAL { get; private set; }
+
+        public Game CurrentGame { get; set; }
+
+        public GameWrapper(BaseDAL dal, Game aGame = null)
+        {
+            DAL = dal;
+
+            if (aGame != null)
+            {
+                CurrentGame = aGame;
+            }
+        }
+     }
+}
