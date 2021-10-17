835dcda87c9e63e919982b7b04b19e6088dd1e98
Jarred Capellman <jcapellman@hotmail.com>
2021-10-17 10:34:28 -0500

    Added: Team Logo loading and selection

--- BravoOne.UWP/ViewModels/NewGameViewModel.cs
+++ BravoOne.UWP/ViewModels/NewGameViewModel.cs
@@ -1,6 +1,12 @@
 ﻿using BravoOne.lib;
 using BravoOne.UWP.ViewModels.Base;
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Windows.Storage;
+
 namespace BravoOne.UWP.ViewModels
 {
     public class NewGameViewModel : BaseViewModel
@@ -33,14 +39,66 @@ namespace BravoOne.UWP.ViewModels
             }
         }
 
+        private List<string> _logos;
+
+        public List<string> Logos
+        {
+            get => _logos;
+
+            set
+            {
+                _logos = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _selectedLogo;
+        
+        public string SelectedLogo
+        {
+            get => _selectedLogo;
+
+            set
+            {
+                _selectedLogo = value;
+                OnPropertyChanged();
+            }
+        }
+
         private void Validate()
         {
             Enabled = !string.IsNullOrEmpty(Name);
         }
 
+        private async void LoadImages()
+        {
+            Logos = new List<string>();
+
+            StorageFolder installedLocation = Windows.ApplicationModel.Package.Current.InstalledLocation;
+
+            StorageFolder subFolder = await installedLocation.GetFolderAsync("Assets");
+            subFolder = await subFolder.GetFolderAsync("Icons");
+
+            var logos = await subFolder.GetFilesAsync();
+
+            var tmpLogos = new List<string>();
+
+            foreach (var logo in logos)
+            {
+                tmpLogos.Add(logo.Path);
+            }
+
+            Logos = tmpLogos;
+
+            SelectedLogo = Logos.FirstOrDefault();
+        }
+
         public NewGameViewModel(GameWrapper gWrapper) : base(gWrapper)
         {
             Name = string.Empty;
+
+
+            LoadImages();
         }
     }
 }
