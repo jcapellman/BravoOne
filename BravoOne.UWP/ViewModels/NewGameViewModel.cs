0a8bd21e6fb6d8f7350e17401b74a6b3fcb7c1cd
Jarred Capellman <jcapellman@hotmail.com>
2021-10-17 09:17:09 -0500

    Added: Additional New Game UI elements

--- BravoOne.UWP/ViewModels/NewGameViewModel.cs
+++ BravoOne.UWP/ViewModels/NewGameViewModel.cs
@@ -5,8 +5,42 @@ namespace BravoOne.UWP.ViewModels
 {
     public class NewGameViewModel : BaseViewModel
     {
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+
+                Validate();
+            }
+        }
+
+        private bool _enabled;
+
+        public bool Enabled
+        {
+            get=> _enabled;
+
+            set
+            {
+                _enabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void Validate()
+        {
+            Enabled = !string.IsNullOrEmpty(Name);
+        }
+
         public NewGameViewModel(GameWrapper gWrapper) : base(gWrapper)
         {
+            Name = string.Empty;
         }
     }
 }
