40dbad7454bdfa1d255de8687aba72cc3dd0edde
Jarred Capellman <jcapellman@hotmail.com>
2021-10-23 09:40:06 -0500

    Added: Initial work on the Credits view

---
+++ BravoOne.UWP/Credits.xaml.cs
@@ -0,0 +1,28 @@
+﻿using BravoOne.lib;
+using BravoOne.UWP.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace BravoOne.UWP
+{
+    public sealed partial class Credits : Page
+    {
+        public Credits()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            DataContext = new CreditsViewModel((GameWrapper)e.Parameter);
+
+            base.OnNavigatedTo(e);
+        }
+
+        private void btnBack_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Frame.GoBack();
+        }
+    }
+}
