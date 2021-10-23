89512a9c1fab3b887ee2b694cfcb572904b9eb22
Jarred Capellman <jcapellman@hotmail.com>
2021-10-23 09:20:45 -0500

    Added: Initial work on the OptionsViewModel class

---
+++ BravoOne.UWP/ViewModels/OptionsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using BravoOne.lib;
+using BravoOne.UWP.ViewModels.Base;
+
+namespace BravoOne.UWP.ViewModels
+{
+    public class OptionsViewModel : BaseViewModel
+    {
+        public lib.Objects.Options Option
+        {
+            get => gWrapper.Option;
+
+            set
+            {
+                gWrapper.Option = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public OptionsViewModel(GameWrapper wrapper) : base(wrapper)
+        {
+            Option = gWrapper.Option;
+        }
+    }
+}
