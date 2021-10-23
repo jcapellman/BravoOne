9ec75f7a59bc55b21ac31c1653af3fab02cdc7bd
Jarred Capellman <jcapellman@hotmail.com>
2021-10-23 09:42:04 -0500

    Added: CreditsViewModel

---
+++ BravoOne.UWP/ViewModels/CreditsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using BravoOne.lib;
+using BravoOne.UWP.ViewModels.Base;
+
+namespace BravoOne.UWP.ViewModels
+{
+    public class CreditsViewModel : BaseViewModel
+    {
+        public CreditsViewModel(GameWrapper wrapper) : base(wrapper)
+        {
+        }
+    }
+}
