ddd02753b2a3fbd712049a5a703167337228f568
Jarred Capellman <jcapellman@hotmail.com>
2021-10-27 20:25:11 -0500

    Added: Credits scrolling

--- BravoOne.UWP/Credits.xaml.cs
+++ BravoOne.UWP/Credits.xaml.cs
@@ -1,6 +1,9 @@
-﻿using BravoOne.lib;
+﻿using System;
+
+using BravoOne.lib;
 using BravoOne.UWP.ViewModels;
 
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
@@ -8,6 +11,9 @@ namespace BravoOne.UWP
 {
     public sealed partial class Credits : Page
     {
+        private DispatcherTimer scroller = new DispatcherTimer();
+        private int margin = 800;
+
         public Credits()
         {
             this.InitializeComponent();
@@ -17,7 +23,23 @@ namespace BravoOne.UWP
         {
             DataContext = new CreditsViewModel((GameWrapper)e.Parameter);
 
+            scroller.Tick += Scroller_Tick;
+            scroller.Interval = new TimeSpan(0, 0, 0, 0, 20);
+            scroller.Start();
+
             base.OnNavigatedTo(e);
+        }
+
+        private void Scroller_Tick(object sender, object e)
+        {
+            margin -= 3;
+
+            gCredits.Margin = new Thickness(0, margin, 0, 0);
+
+            if (margin <= 30)
+            {
+                scroller.Stop();
+            }
         }
 
         private void btnBack_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
