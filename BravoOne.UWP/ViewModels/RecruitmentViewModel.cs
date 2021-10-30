6d5a96b4b71549ef4c7a1a3b24b80c6d1c99f7f2
Jarred Capellman <jcapellman@hotmail.com>
2021-10-29 21:56:22 -0500

    Added: Recruitment binding and functionality

--- BravoOne.UWP/ViewModels/RecruitmentViewModel.cs
+++ BravoOne.UWP/ViewModels/RecruitmentViewModel.cs
@@ -1,12 +1,47 @@
 ﻿using BravoOne.lib;
+using BravoOne.lib.Objects;
 using BravoOne.UWP.ViewModels.Base;
+
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BravoOne.UWP.ViewModels
 {
     public class RecruitmentViewModel : BaseViewModel
     {
+        private List<TeamMember> _recruits;
+
+        public List<TeamMember> Recruits
+        {
+            get => _recruits;
+
+            set
+            {
+                _recruits = value;
+
+                OnPropertyChanged();
+            }
+        }
+
         public RecruitmentViewModel(GameWrapper gWrapper) : base(gWrapper)
         {
+            LoadRecruits();
+        }
+
+        private void LoadRecruits()
+        {
+            Recruits = gWrapper.CurrentGame.TeamMembers.Where(a => !a.OnTeam).OrderByDescending(b => b.MonthlySalary).ToList();
+        }
+
+        public void AddTeamMember(TeamMember recruit)
+        {
+            var index = gWrapper.CurrentGame.TeamMembers.IndexOf(recruit);
+
+            recruit.OnTeam = true;
+
+            gWrapper.CurrentGame.TeamMembers[index] = recruit;
+
+            LoadRecruits();
         }
     }
 }
