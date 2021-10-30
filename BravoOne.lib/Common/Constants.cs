cdef6a0adbeec964653a085086d123542469829a
Jarred Capellman <jcapellman@hotmail.com>
2021-10-29 21:54:11 -0500

    Added: Initial list of First and Last Names

---
+++ BravoOne.lib/Common/Constants.cs
@@ -0,0 +1,51 @@
+﻿namespace BravoOne.lib.Common
+{
+    public class Constants
+    {
+        public static string[] FIRST_NAMES = new string[]
+        {
+            "JOHN",
+            "JAMES",
+            "JANE",
+            "SANDRA",
+            "KC",
+            "AMY",
+            "JARRED",
+            "PATRICK",
+            "MILES",
+            "NOPHIL",
+            "DREW",
+            "ROB",
+            "PERRY",
+            "NICK",
+            "AVIN",
+            "DAK",
+            "ALEJANDRO",
+            "CLAY",
+            "ANDREI",
+            "WIL"
+        };
+
+        public static string[] LAST_NAMES = new string[]
+        {
+            "ANDERSON",
+            "RAMBO",
+            "MCLANE",
+            "PLISKEN",
+            "SMITH",
+            "WILLIAMS",
+            "JONES",
+            "MILLER",
+            "DAVIS",
+            "JOHNSON",
+            "GARCIA",
+            "MARTINEZ",
+            "RODRIGUEZ",
+            "WANG",
+            "WILSON",
+            "LEWIS",
+            "HILL",
+            "SCOTT"
+        };
+    }
+}
