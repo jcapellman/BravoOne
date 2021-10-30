bbaebebcaa67dcbf71e3954e2ed5d9ea5082c39a
Jarred Capellman <jcapellman@hotmail.com>
2021-10-30 08:32:52 -0500

    Added: Comments and invalidation of ability to recruit

--- BravoOne.UWP/ViewModels/RecruitmentViewModel.cs
+++ BravoOne.UWP/ViewModels/RecruitmentViewModel.cs
@@ -30,7 +30,28 @@ namespace BravoOne.UWP.ViewModels
 
         private void LoadRecruits()
         {
-            Recruits = gWrapper.CurrentGame.TeamMembers.Where(a => !a.OnTeam).OrderByDescending(b => b.MonthlySalary).ToList();
+            var availableMembers = gWrapper.CurrentGame.TeamMembers.Where(a => !a.OnTeam).OrderByDescending(b => b.MonthlySalary).ToList();
+
+            Recruits = new List<TeamMember>();
+
+            foreach (var recruit in availableMembers)
+            {
+                recruit.OnTeam = true;
+                recruit.Comments = "Available for your team";
+
+                if (recruit.SkillPoints > gWrapper.CurrentGame.TeamLevel)
+                {
+                    recruit.Comments = "Recruit is too experienced for your team";
+                    recruit.OnTeam = false;
+                }
+                else if (recruit.MonthlySalary > gWrapper.CurrentGame.Money)
+                {
+                    recruit.Comments = "Recruit is too expensive for your team";
+                    recruit.OnTeam = false;
+                }
+
+                Recruits.Add(recruit);
+            }
         }
 
         public void AddTeamMember(TeamMember recruit)
