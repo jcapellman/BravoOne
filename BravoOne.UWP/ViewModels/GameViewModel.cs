1431f9d551ce6331d924dd180a997ebfcc969dd0
Jarred Capellman <jcapellman@hotmail.com>
2021-10-29 21:55:46 -0500

    Added: TeamMember collection and filter in the GameViewModel

--- BravoOne.UWP/ViewModels/GameViewModel.cs
+++ BravoOne.UWP/ViewModels/GameViewModel.cs
@@ -1,13 +1,31 @@
 ﻿using BravoOne.lib;
-
+using BravoOne.lib.Objects;
 using BravoOne.UWP.ViewModels.Base;
+
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BravoOne.UWP.ViewModels
 {
     public class GameViewModel : BaseViewModel
     {
+        private List<TeamMember> _teamMembers;
+
+        public List<TeamMember> TeamMembers
+        {
+            get => _teamMembers;
+
+            set
+            {
+                _teamMembers = value;
+
+                OnPropertyChanged();
+            }
+        }
+
         public GameViewModel(GameWrapper gWrapper) : base(gWrapper)
         {
+            TeamMembers = gWrapper.CurrentGame.TeamMembers.Where(a => a.OnTeam).OrderBy(b => b.Name).ToList();
         }
 
         public void SaveGame()
