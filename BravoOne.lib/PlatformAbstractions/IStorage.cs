ee85e90c7868058e8bcd0afc08b2408adcf45fde
Jarred Capellman <jcapellman@hotmail.com>
2021-10-31 16:01:41 -0500

    Added: IStorage Interface for Avatars

---
+++ BravoOne.lib/PlatformAbstractions/IStorage.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BravoOne.lib.PlatformAbstractions
+{
+    public interface IStorage
+    {
+        Task<List<string>> GetAvatarImagesAsync();
+    }
+}
