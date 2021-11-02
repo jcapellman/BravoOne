43a43b954f008516c2345be416ce371fafdaebde
Jarred Capellman <jcapellman@hotmail.com>
2021-11-01 20:12:13 -0500

    Added: Initial work on the EndGameViewModel class

---
+++ BravoOne.UWP/ViewModels/EndGameViewModel.cs
@@ -0,0 +1,12 @@
+﻿using BravoOne.lib;
+using BravoOne.UWP.ViewModels.Base;
+
+namespace BravoOne.UWP.ViewModels
+{
+    public class EndGameViewModel : BaseViewModel
+    {
+        public EndGameViewModel(GameWrapper wrapper) : base(wrapper)
+        {
+        }
+    }
+}
