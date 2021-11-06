9e161354b1b02e4807db82fcc19dc4b0fb9d3410
Jarred Capellman <jcapellman@hotmail.com>
2021-11-06 18:20:36 -0500

    Added: Initial Achievements View

---
+++ BravoOne.UWP/Achievements.xaml.cs
@@ -0,0 +1,28 @@
+﻿using BravoOne.lib;
+using BravoOne.UWP.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace BravoOne.UWP
+{
+    public sealed partial class Achievements : Page
+    {
+        public Achievements()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            DataContext = new AchievementsViewModel((GameWrapper)e.Parameter);
+
+            base.OnNavigatedTo(e);
+        }
+
+        private void btnBack_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Frame.GoBack();
+        }
+    }
+}
