8057a7a290d8a79b78006fe10b3509c84b7c494b
Jarred Capellman <jcapellman@hotmail.com>
2021-11-06 16:12:50 -0500

    Added: Initial work on the EquipmentViewModel

--- BravoOne.UWP/ViewModels/EquipmentViewModel.cs
+++ BravoOne.UWP/ViewModels/EquipmentViewModel.cs
@@ -6,6 +6,22 @@ namespace BravoOne.UWP.ViewModels
     {
         public EquipmentViewModel(GameWrapper wrapper) : base(wrapper)
         {
+            InitializeEquipment();
+        }
+
+        private void InitializeEquipment()
+        {
+            foreach (var equipment in gWrapper.CurrentGame.AvailableEquipment)
+            {
+                equipment.Available = true;
+                equipment.Comments = string.Empty;
+
+                if (equipment.RequiredLevel > gWrapper.CurrentGame.TeamLevel)
+                {
+                    equipment.Available = false;
+                    equipment.Comments = "Not skilled enough";
+                }
+            }
         }
     }
 }
