b57060db776b53192bb72abe979fea9086e9db58
Jarred Capellman <jcapellman@hotmail.com>
2021-11-06 09:01:49 -0500

    Added: TeamMemberStatus Enum

---
+++ BravoOne.lib/Enums/TeamMemberStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace BravoOne.lib.Enums
+{
+    public enum TeamMemberStatus
+    {
+        OnTeam,
+        Available,
+        OnContract,
+        Injured,
+        Deceased,
+        Retired
+    }
+}
