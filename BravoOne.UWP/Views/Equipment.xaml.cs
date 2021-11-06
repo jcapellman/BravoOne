7cbc155001a53808400a079c2295639086ffe6d7
Jarred Capellman <jcapellman@hotmail.com>
2021-11-06 16:00:52 -0500

    Added: Initial work on the Equipment View

---
+++ BravoOne.UWP/Views/Equipment.xaml.cs
@@ -0,0 +1,30 @@
+﻿using BravoOne.lib;
+using BravoOne.UWP.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace BravoOne.UWP.Views
+{
+    public sealed partial class Equipment : Page
+    {
+        private EquipmentViewModel ViewModel => (EquipmentViewModel)DataContext;
+
+        public Equipment()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            DataContext = new EquipmentViewModel((GameWrapper)e.Parameter);
+
+            base.OnNavigatedTo(e);
+        }
+
+        private void btnBack_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage), ViewModel.gWrapper);
+        }
+    }
+}
