6919b7bf62303090c3c643044fb8da3f96741716
Jarred Capellman <jcapellman@hotmail.com>
2021-11-06 09:54:25 -0500

    Added: Equipment Class

---
+++ BravoOne.lib/Objects/Equipment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using BravoOne.lib.Enums;
+
+namespace BravoOne.lib.Objects
+{
+    public class Equipment
+    {
+        public Guid id { get; set; }
+
+        public string Name { get; set; }
+
+        public int RequiredLevel { get; set; }
+
+        public int Cost { get; set; }
+
+        public EquipmentType EType { get; set; }
+
+        public int Reliability { get; set; }
+
+        public int Damage { get; set; }
+    }
+}
