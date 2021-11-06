3bef0b27d8f2953f52a6f116ed235b4ec0c54e21
Jarred Capellman <jcapellman@hotmail.com>
2021-11-06 09:54:43 -0500

    Added: TeamEquipment Class

---
+++ BravoOne.lib/Objects/TeamEquipment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BravoOne.lib.Objects
+{
+    public class TeamEquipment
+    {
+        public Guid Id { get; set; }
+
+        public int Status { get; set; }
+
+        public Guid EquipmentId { get; set; }
+    }
+}
