94c37b0ed2ee1648ba6a6571698a53dd67225b20
Jarred Capellman <jcapellman@hotmail.com>
2021-11-06 17:28:08 -0500

    Added: Support to purchase equipment and cost checks in the EquipmentViewModel

--- BravoOne.UWP/ViewModels/EquipmentViewModel.cs
+++ BravoOne.UWP/ViewModels/EquipmentViewModel.cs
@@ -19,9 +19,20 @@ namespace BravoOne.UWP.ViewModels
                 if (equipment.RequiredLevel > gWrapper.CurrentGame.TeamLevel)
                 {
                     equipment.Available = false;
-                    equipment.Comments = "Not skilled enough";
+                    equipment.Comments = "Equipment experience is too much for your team";
+                } else if (equipment.Cost > gWrapper.CurrentGame.Money)
+                {
+                    equipment.Comments = "Equipment is too expensive for your team";
+                    equipment.Available = false;
                 }
             }
+        }
+
+        internal void AddEquipment(lib.Objects.Equipment equipment)
+        {
+            gWrapper.CurrentGame.AddEquipment(equipment);
+
+            InitializeEquipment();
         }
     }
 }
