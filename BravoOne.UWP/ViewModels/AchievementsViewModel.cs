5445afab7046bb92377a541abbc38b4039423010
Jarred Capellman <jcapellman@hotmail.com>
2021-11-06 18:20:16 -0500

    Added: Initial work on the AchievementsViewModel

---
+++ BravoOne.UWP/ViewModels/AchievementsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using BravoOne.lib;
+using BravoOne.UWP.ViewModels.Base;
+
+namespace BravoOne.UWP.ViewModels
+{
+    public class AchievementsViewModel : BaseViewModel
+    {
+        public AchievementsViewModel(GameWrapper wrapper) : base(wrapper)
+        {
+        }
+    }
+}
