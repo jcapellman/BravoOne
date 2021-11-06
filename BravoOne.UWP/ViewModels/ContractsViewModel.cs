293224fccf4f05dcd16ccdb480f92cac3c3539f7
Jarred Capellman <jcapellman@hotmail.com>
2021-11-06 18:10:23 -0500

    Added: Initial work on the ContractsViewModel

---
+++ BravoOne.UWP/ViewModels/ContractsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using BravoOne.lib;
+using BravoOne.lib.Objects;
+using BravoOne.UWP.ViewModels.Base;
+
+namespace BravoOne.UWP.ViewModels
+{
+    public class ContractsViewModel : BaseViewModel
+    {
+        public ContractsViewModel(GameWrapper wrapper) : base(wrapper)
+        {
+        }
+
+        public void AcceptContract(Contract contract)
+        {
+            gWrapper.CurrentGame.AddContract(contract);
+        }
+    }
+}
