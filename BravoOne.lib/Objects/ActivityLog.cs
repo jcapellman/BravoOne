3eca8e3129cdb3b05e147e6c4ff83d1ad1793f43
Jarred Capellman <jcapellman@hotmail.com>
2021-11-10 10:15:40 -1000

    Added: ActivityLog Class

---
+++ BravoOne.lib/Objects/ActivityLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using BravoOne.lib.Enums;
+
+namespace BravoOne.lib.Objects
+{
+    public class ActivityLog
+    {
+        public Guid Id { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        public ActivityType ActivityLogType { get; set; }
+
+        public string Comments { get; set; }
+    }
+}
