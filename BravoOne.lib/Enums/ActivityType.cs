671497250d93e6e4d45411776a1d261183ff6e56
Jarred Capellman <jcapellman@hotmail.com>
2021-11-10 10:15:29 -1000

    Added: ActivityType class object

---
+++ BravoOne.lib/Enums/ActivityType.cs
@@ -0,0 +1,12 @@
+﻿namespace BravoOne.lib.Enums
+{
+    public enum ActivityType
+    {
+        TEAM_MEMBER_LEVEL_UP,
+        TEAM_MEMBER_DIED,
+        TEAM_MEMBER_RETIRED,
+        CONTRACT_FAILED,
+        CONTRACT_COMPLETED,
+        TEAM_LEVEL_UP
+    }
+}
