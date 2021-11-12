5293689229782f6a97c0319d11565f0ef0cdb368
Jarred Capellman <jcapellman@hotmail.com>
2021-11-12 09:55:43 -1000

    Added: UI Object for Achievement Listing

---
+++ BravoOne.lib/UIObjects/AchievementsListingItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BravoOne.lib.UIObjects
+{
+    public class AchievementsListingItem
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool Unlocked { get; set; }
+
+        public DateTime? TimeStamp { get; set; }
+    }
+}
