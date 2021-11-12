3de82a9aa71bdd3b8b72b0a83f196d2425ab5569
Jarred Capellman <jcapellman@hotmail.com>
2021-11-12 08:30:51 -1000

    Added: HireTeamMember Achievement

---
+++ BravoOne.lib/Achievements/HireTeamMember.cs
@@ -0,0 +1,25 @@
+﻿using BravoOne.lib.Achievements.Base;
+using BravoOne.lib.Objects;
+
+namespace BravoOne.lib.Achievements
+{
+    public class HireTeamMember : BaseAchievement
+    {
+        
+        public override string Title => "Mereceny for Hire";
+
+        public override string Description => "Hired a team member";
+
+        public override int Id => 1;
+
+        public override bool VerifyAchievement(Game currentGame)
+        {
+            if (currentGame.TeamMembers.Count > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
