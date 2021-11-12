e983a1def280cb1b142a142ee98cdeeacea077ad
Jarred Capellman <jcapellman@hotmail.com>
2021-11-12 07:15:50 -1000

    Added: Initial commit of BaseAchievement class

---
+++ BravoOne.lib/Achievements/Base/BaseAchievement.cs
@@ -0,0 +1,13 @@
+﻿using BravoOne.lib.Objects;
+
+namespace BravoOne.lib.Achievements.Base
+{
+    public abstract class BaseAchievement
+    {
+        public abstract string Title { get; }
+
+        public abstract string Description { get; }
+
+        public abstract void VerifyAchievement(Game currentGame);
+    }
+}
