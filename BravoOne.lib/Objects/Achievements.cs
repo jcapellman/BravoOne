1d12fff376bd9ca05a0cd34ccd069e958f1ecc9e
Jarred Capellman <jcapellman@hotmail.com>
2021-11-12 08:31:10 -1000

    Added: Achievements class object

---
+++ BravoOne.lib/Objects/Achievements.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BravoOne.lib.Objects
+{
+    public class Achievements
+    {
+        public Guid Id { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+        
+        public int AchievementId { get; set;  }
+    }
+}
