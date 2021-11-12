62d2a109fe5ac49de40453e276773a8f84def3ab
Jarred Capellman <jcapellman@hotmail.com>
2021-11-12 09:31:10 -1000

    Added: BaseManager class

---
+++ BravoOne.lib/Managers/Base/BaseManager.cs
@@ -0,0 +1,14 @@
+﻿using BravoOne.lib.PlatformAbstractions;
+
+namespace BravoOne.lib.Managers.Base
+{
+    public class BaseManager
+    {
+        protected IStorage Storage;
+
+        protected BaseManager(IStorage storage)
+        {
+            Storage = storage;
+        }
+    }
+}
