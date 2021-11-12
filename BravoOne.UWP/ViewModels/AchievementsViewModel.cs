1246bc53d9884a6e5820d83839299104020453a2
Jarred Capellman <jcapellman@hotmail.com>
2021-11-12 09:59:12 -1000

    Added: AchievementListing logic to the AchievementViewModel

--- BravoOne.UWP/ViewModels/AchievementsViewModel.cs
+++ BravoOne.UWP/ViewModels/AchievementsViewModel.cs
@@ -1,12 +1,31 @@
 ﻿using BravoOne.lib;
+using BravoOne.lib.Managers;
+using BravoOne.lib.UIObjects;
 using BravoOne.UWP.ViewModels.Base;
+
+using System.Collections.Generic;
 
 namespace BravoOne.UWP.ViewModels
 {
     public class AchievementsViewModel : BaseViewModel
     {
+        private List<AchievementsListingItem> _achievementListing;
+
+        public List<AchievementsListingItem> AchievementListing
+        {
+            get => _achievementListing;
+
+            set
+            {
+                _achievementListing = value;
+
+                OnPropertyChanged();
+            }
+        }
+
         public AchievementsViewModel(GameWrapper wrapper) : base(wrapper)
         {
+            AchievementListing = new AchievementManager(wrapper.Storage, wrapper.DAL).GetAchievementListing();
         }
     }
 }
