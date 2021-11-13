754a2414754c6654850f61f3bbdba6dfb07a0178
Jarred Capellman <jcapellman@hotmail.com>
2021-11-13 08:54:21 -1000

    Added: Initial work on the TeamMemberManager

---
+++ BravoOne.lib/Managers/TeamMemberManager.cs
@@ -0,0 +1,32 @@
+﻿using BravoOne.lib.Enums;
+using BravoOne.lib.Objects;
+using BravoOne.lib.PlatformAbstractions;
+using BravoOne.lib.DAL.Base;
+using BravoOne.lib.Managers.Base;
+
+using System.Linq;
+
+namespace BravoOne.lib.Managers
+{
+    public class TeamMemberManager : BaseManager
+    {
+        public TeamMemberManager(IStorage storage, BaseDAL dal) : base(storage, dal)
+        {
+        }
+
+        public override (TurnStatus Status, Game CurrentGame) ProcessTurn(Game currentGame)
+        {
+            foreach (TeamMember member in currentGame.TeamMembers.Where(a => a.Status == TeamMemberStatus.OnTeam))
+            {
+                if (member.MonthlySalary > currentGame.Money)
+                {
+                    return (TurnStatus.OUT_OF_MONEY, currentGame);
+                }
+
+                currentGame.Money -= member.MonthlySalary;
+            }
+
+            return (TurnStatus.OK, currentGame);
+        }
+    }
+}
