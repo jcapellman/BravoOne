ef3243d7e69656c150e31c8113be933d9f78e37d
Jarred Capellman <jcapellman@hotmail.com>
2021-11-20 08:37:53 -0600

    Added: Initial work on the Contract Initialization

--- BravoOne.lib/Managers/ContractManager.cs
+++ BravoOne.lib/Managers/ContractManager.cs
@@ -18,12 +18,31 @@ namespace BravoOne.lib.Managers
 
         public override async Task<Game> InitializeAsync(Game currentGame)
         {
-            if (currentGame.Contracts != null)
-            {
-                return currentGame;
-            }
+            currentGame.AvailableContracts = new System.Collections.ObjectModel.ObservableCollection<Contract>();
 
-            currentGame.Contracts = new System.Collections.ObjectModel.ObservableCollection<Contract>();
+            var randomName = new Random((int)DateTime.Now.Ticks);
+            var randomPrefix = new Random((int)DateTime.Now.Ticks+1);
+
+            for (var x = 0; x < 10; x++)
+            {
+                var contract = new Contract
+                {
+                    Id = Guid.NewGuid(),
+                    Status = ContractStatus.NotStarted,
+                    AssignedTeamMembers = new System.Collections.Generic.List<Guid>()
+                };
+
+                do
+                {
+                    var name = Common.Constants.MISSION_NAMES[randomName.Next(0, Common.Constants.MISSION_NAMES.Length - 1)];
+                    var prefix = Common.Constants.MISSION_PREFIX[randomPrefix.Next(0, Common.Constants.MISSION_PREFIX.Length - 1)];
+
+                    contract.Name = $"{prefix} {name}";
+                } while (currentGame.AvailableContracts.Any(a => a.Name == contract.Name) || 
+                    currentGame.Contracts.Any(a => a.Name == contract.Name));
+
+                currentGame.AvailableContracts.Add(contract);
+            }
 
             return currentGame;
         }
