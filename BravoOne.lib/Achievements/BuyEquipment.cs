c940fe542291bcc80ee925388c435551163d95f6
Jarred Capellman <jcapellman@hotmail.com>
2021-11-24 08:48:19 -0600

    Added: BuyEquipment Achievement

---
+++ BravoOne.lib/Achievements/BuyEquipment.cs
@@ -0,0 +1,22 @@
+﻿using BravoOne.lib.Achievements.Base;
+using BravoOne.lib.Objects;
+
+namespace BravoOne.lib.Achievements
+{
+    public class BuyEquipment : BaseAchievement
+    {
+        public override string Title => "Equipment Purchaser";
+
+        public override string Description => "Bought your first equipment";
+
+        public override bool VerifyAchievement(Game currentGame)
+        {
+            if (currentGame.TeamEquipment.Count > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
