f3437430bcaf4e88136108af9164f03b582d4b21
Jarred Capellman <jcapellman@hotmail.com>
2021-11-25 08:56:31 -0600

    Added: HireTenTeamMembers Achievement

---
+++ BravoOne.lib/Achievements/HireTenTeamMembers.cs
@@ -0,0 +1,23 @@
+﻿using BravoOne.lib.Achievements.Base;
+using BravoOne.lib.Objects;
+
+namespace BravoOne.lib.Achievements
+{
+    public class HireTenTeamMembers : BaseAchievement
+    {
+
+        public override string Title => "Hiring Manager";
+
+        public override string Description => "Hired 10 team members";
+
+        public override bool VerifyAchievement(Game currentGame)
+        {
+            if (currentGame.TeamMembers.Count > 10)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
