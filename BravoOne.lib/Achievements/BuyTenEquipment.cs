16c6eeee5771060e73faeab86f04fede8de0a598
Jarred Capellman <jcapellman@hotmail.com>
2021-11-26 09:42:53 -0600

    Added: BuyTenEquipment Achievement

---
+++ BravoOne.lib/Achievements/BuyTenEquipment.cs
@@ -0,0 +1,22 @@
+﻿using BravoOne.lib.Achievements.Base;
+using BravoOne.lib.Objects;
+
+namespace BravoOne.lib.Achievements
+{
+    internal class BuyTenEquipment : BaseAchievement
+    {
+        public override string Title => "Arms Dealer";
+
+        public override string Description => "Owned ten pieces of equipment";
+
+        public override bool VerifyAchievement(Game currentGame)
+        {
+            if (currentGame.TeamEquipment.Count >= 10)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
