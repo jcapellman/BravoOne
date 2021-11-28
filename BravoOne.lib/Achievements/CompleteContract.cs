b2b123fb41cb5792e18ee929de4683f5cfd18369
Jarred Capellman <jcapellman@hotmail.com>
2021-11-28 15:11:23 -0600

    Added: CompleteContract achievement

---
+++ BravoOne.lib/Achievements/CompleteContract.cs
@@ -0,0 +1,16 @@
+﻿using BravoOne.lib.Achievements.Base;
+using BravoOne.lib.Objects;
+
+using System.Linq;
+
+namespace BravoOne.lib.Achievements
+{
+    public class CompleteContract : BaseAchievement
+    {
+        public override string Title => "Completed a Gig";
+
+        public override string Description => "Completed your first contract";
+
+        public override bool VerifyAchievement(Game currentGame) => currentGame.Contracts.Any(a => a.Status == Enums.ContractStatus.Completed);
+    }
+}
