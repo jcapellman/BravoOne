e3f2cfc1db07e5e178ef689f6100ac2d17076122
Jarred Capellman <jcapellman@hotmail.com>
2021-11-29 19:15:07 -0600

    Added: CompleteTenContracts achievement

---
+++ BravoOne.lib/Achievements/CompleteTenContracts.cs
@@ -0,0 +1,16 @@
+﻿using BravoOne.lib.Achievements.Base;
+using BravoOne.lib.Objects;
+
+using System.Linq;
+
+namespace BravoOne.lib.Achievements
+{
+    public class CompleteTenContracts : BaseAchievement
+    {
+        public override string Title => "Contract King";
+
+        public override string Description => "Complete 10 Contracts";
+
+        public override bool VerifyAchievement(Game currentGame) => currentGame.Contracts.Count(a => a.Status == Enums.ContractStatus.Completed) > 10;
+    }
+}
