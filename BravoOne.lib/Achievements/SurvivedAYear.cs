3b4a668c7e1f3f69c543676bfc6af534c51c6fc8
Jarred Capellman <jcapellman@hotmail.com>
2021-12-28 08:12:01 -0600

    Added: SurvivedAYear achievement

---
+++ BravoOne.lib/Achievements/SurvivedAYear.cs
@@ -0,0 +1,22 @@
+﻿using BravoOne.lib.Achievements.Base;
+using BravoOne.lib.Objects;
+
+namespace BravoOne.lib.Achievements
+{
+    public class SurvivedAYear : BaseAchievement
+    {
+        public override string Title => "Survivor for a Year";
+
+        public override string Description => "Lasted a year";
+
+        public override bool VerifyAchievement(Game currentGame)
+        {
+            if (currentGame.gsMonths == 12)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
