70cb7426b766b03a171f7b84747f59ce6158d48d
Jarred Capellman <jcapellman@hotmail.com>
2022-01-22 19:04:44 -0600

    Added: 10 Year Achievement

---
+++ BravoOne.lib/Achievements/SurvivedTenYears.cs
@@ -0,0 +1,22 @@
+﻿using BravoOne.lib.Achievements.Base;
+using BravoOne.lib.Objects;
+
+namespace BravoOne.lib.Achievements
+{
+    public class SurvivedTenYears : BaseAchievement
+    {
+        public override string Title => "Survivor for a 10 Years";
+
+        public override string Description => "Lasted 10 years";
+
+        public override bool VerifyAchievement(Game currentGame)
+        {
+            if (currentGame.gsMonths == 120)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
